a974e93b23432bf01fabf799bf8ae025ca3e87d9
FroniWahyudi <froniwahyudi@gmail.com>
2025-04-28 08:36:43 +0700

    update modul hal.82

--- resources/views/backend/v_layouts/app.blade.php
+++ resources/views/backend/v_layouts/app.blade.php
@@ -234,40 +234,8 @@ class="sidebar-link"><i class="mdi mdi-chevron-right"></i><span class="hide-menu
 <!-- @yieldAwal -->
 @yield('content')
 <!-- @yieldAkhir-->
-<div class="row">
-<div class="col-12">
-<div class="card">
-<div class="card-body">
-<h5 class="card-title">Basic Datatable</h5>
-<div class="table-responsive">
-<table id="zero_config" class="table table-striped
-table-bordered">
-<thead>
-<tr>
-<th>Name</th>
-<th>Position</th>
-<th>Office</th>
-<th>Age</th>
-<th>Start date</th>
-<th>Salary</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>Tiger Nixon</td>
-<td>System Architect</td>
-<td>Edinburgh</td>
-<td>61</td>
-<td>2011/04/25</td>
-<td>$320,800</td>
-</tr>
-</tbody>
-</table>
-</div>
-</div>
-</div>
-</div>
-</div>
+
+
 <!-- ============================================================== -->
 <!-- End PAge Content -->
 <!-- ============================================================== -->
