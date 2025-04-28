462b2364beb6e1b850c2ab8b16d27a8321a82305
FroniWahyudi <froniwahyudi@gmail.com>
2025-04-28 19:29:16 +0700

    update modul hal.125 ,,perbarui kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -53,22 +53,35 @@ class KategoriController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $kategori = Kategori::find($id);
+        return view('backend.v_kategori.edit', [
+        'judul' => 'Kategori',
+        'edit' => $kategori
+        ]);
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
+/**
+* Update the specified resource in storage.
+*/
     public function update(Request $request, string $id)
     {
-        //
+        $rules = [
+        'nama_kategori' => 'required|max:255|unique:kategori,nama_kategori,' . $id,
+        ];
+        $validatedData = $request->validate($rules);
+        Kategori::where('id', $id)->update($validatedData);
+        return redirect()->route('backend.kategori.index')->with('success', 'Data berhasil
+        diperbaharui');
     }
+
 
     /**
      * Remove the specified resource from storage.
      */
     public function destroy(string $id)
     {
-        //
+        $kategori = kategori::findOrFail($id);
+        $kategori ->delete();
+        return redirect()->route('backend.kategori.index')->with('success', 'Data berhasil
+        dihapus');
     }
 }
