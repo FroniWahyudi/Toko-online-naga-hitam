89dbdb0c288be0150bb868d28eb3aff79c48bbfd
FroniWahyudi <froniwahyudi@gmail.com>
2025-04-29 13:53:15 +0700

    mundur ke hal 120lewat merge

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class BerandaController extends Controller
 {
-    public function berandaBackend()
+    public function index()
     {
         return view('backend.v_beranda.index', [
              'judul' => 'Halaman Beranda',
